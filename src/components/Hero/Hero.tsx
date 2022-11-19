417c26ba48b74298de85f2ad7e0b3e3563aa9d28
devHectorGa <contacto@devHector.Ga>
2022-11-19 12:07:36 -0500

    Hero Component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,21 @@
-import { Typography } from "antd";
-import { HeroContainer } from "./Hero.styles";
+import { Button, Typography } from "antd";
+import { HeroContainer, HeroContent } from "./Hero.styles";
+import { Waves } from "./Waves";
 
-export const Hero = () => {
-  return (
-    <HeroContainer>
-      <Typography.Title>Título</Typography.Title>
-    </HeroContainer>
-  );
-};
+const { Title, Paragraph } = Typography;
+
+export const Hero = () => (
+  <HeroContainer>
+    <HeroContent>
+      <Title>Eden del Parque</Title>
+      <Paragraph>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </Paragraph>
+      <Button type="primary">Leer mas...</Button>
+    </HeroContent>
+    <Waves />
+  </HeroContainer>
+);
